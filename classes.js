2dd60b5a4e313c76cba4fdeed5e09eede877bd6c
Jung Eun (Jessica) Kwak <jkwak39@gmail.com>
2020-02-17 13:04:40 +0800

    030304

--- classes.js
+++ classes.js
@@ -31,3 +31,23 @@ class Car {
 
 let myCar = new Car("Ford", "Focus", "Black");
 console.log(myCar);
+
+//030304
+class Animal {
+  constructor(name, verb, sound = "") {
+    this.name = name;
+    this.verb = verb;
+    this.sound = sound;
+  }
+  speak() {
+    return this.sound == ""
+      ? `${this.name} ${this.verb}`
+      : `${this.name} says '${this.sound}'`;
+  }
+}
+
+console.log(new Animal("Dog", "barks").speak());
+console.log(new Animal("Cat", "meows").speak());
+console.log(new Animal("Cow", "eats", "moo").speak());
+console.log(new Animal("Lion", "roars").speak());
+console.log(new Animal("Duck", "plays", "quack").speak());
