dec7404504997a9f186ca4a1395e61807064c0e9
Jung Eun (Jessica) Kwak <jkwak39@gmail.com>
2020-02-17 12:29:23 +0800

    030302

--- classes.js
+++ classes.js
@@ -7,3 +7,15 @@ class Bike {
 
 let myBike = new Bike();
 console.log(myBike);
+
+//030302
+class Student {
+  constructor(fName, lName, age) {
+    this.firstName = fName;
+    this.lastName = lName;
+    this.age = age;
+  }
+}
+
+let newStudent = new Student("John", "Smith", 18);
+console.log(newStudent);
